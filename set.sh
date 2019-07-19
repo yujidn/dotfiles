d51e610a81568f83a07234d4470b2d843de24fd9
yuji nunome <nunome@kousokuya.co.jp>
2019-07-19 15:43:31 +0900

    plugのセットアップを追加

--- set.sh
+++ set.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 ln -s ./.bashrc ~/.bashrc
-ln -s ./.vimrc ~/.vimrc
-mkdir ~/.config/nvim
 
+
+## vim setup
+ln -s ./.vimrc ~/.vimrc
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+## nvim setup
+mkdir ~/.config/nvim
 ln -s ./.vimrc ~/.config/nvim/init.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
